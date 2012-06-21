c7ec3f03a8f0ca465f08883f936aeae59555057c
FUJITA Setsu <fjt.seth@gmail.com>
2012-06-21 11:07:54 +0900

    write tsv not xml.

--- src/main/scala/com/github/sett4/descgpx/App.scala
+++ src/main/scala/com/github/sett4/descgpx/App.scala
@@ -13,6 +13,8 @@ import scala.concurrent.SyncVar
 import scala.xml._
 import scala.xml.parsing._
 import scala.xml.pull._
+import java.text.{DateFormat, SimpleDateFormat}
+import java.util
 
 object App {
   def main(args: Array[String]): Unit = {
@@ -25,16 +27,26 @@ object App {
 }
 
 class GpxReader(val reader: XMLEventReader) extends HalfEventParser(reader) {
-  def acceptPf = {
-    case e @ EvElemStart(_, "metadata", _, _) => onMetadata
-    case e @ EvElemStart(_, "trkpt", _, _) => onTrkpt
+
+  def read() = {
+    super.read({
+      case e @ EvElemStart(_, "metadata", _, _) => onMetadata
+      case e @ EvElemStart(_, "trkpt", _, _) => onTrkpt
+    });
   }
 
   def onMetadata(elem: Elem) = {
-    println(elem.toString())
+//    println(elem.toString())
   }
 
+  val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+  dateFormat.setTimeZone(util.TimeZone.getTimeZone("UTC"))
   def onTrkpt(elem: Elem) = {
-    println(elem.toString())
+    val time = dateFormat.parse((elem \\ "time").text)
+    val lon = elem.attribute("lon").getOrElse("")
+    val lat = elem.attribute("lat").getOrElse("")
+    val ele = (elem \\ "ele").text
+    val cad = (elem \\ "cad").text
+    println("%s\t%s\t%s\t%s\t%s".format(time, lon, lat, ele, cad))
   }
 }
